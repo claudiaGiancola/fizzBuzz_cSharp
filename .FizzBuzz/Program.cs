ecacad87fd791007f307fc90112a9ecf4a5e23ed
claudiaGiancola <cloemezzaluna@gmail.com>
2024-08-05 11:14:18 +0100

    switch case with when to print fizz, buzz and fizzbuzz

--- .FizzBuzz/Program.cs
+++ .FizzBuzz/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to FizzBuzz!");
+
+string fizz = "Fizz";
+string buzz = "Buzz";
+
+for (int i = 1; i <= 100; i++)
+{
+    switch (true)
+    {
+        case true when i % 3 == 0 && i % 5 == 0:
+            Console.WriteLine(fizz + buzz);
+            break;
+
+        case true when i % 3 == 0:
+            Console.WriteLine(fizz);
+            break;
+
+        case true when i % 5 == 0:
+            Console.WriteLine(buzz);
+            break;
+
+        default:
+            Console.WriteLine(i);
+            break;
+    }
+}
